1fc0702c2e378f4126c9cc0dcdbc8018230f269a
root <root@7l2.aliyun>
2020-05-08 21:09:52 +0800

    771.宝石与石头

---
+++ 771_宝石与石头.java
@@ -0,0 +1,14 @@
+//way1 
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int res = 0;
+        for(int i = 0; i < J.length(); ++i){
+            for(int j = 0; j < S.length(); ++j){
+                if(J.charAt(i) == S.charAt(j)){
+                    ++res;
+                }
+            }
+        }
+        return res;
+    }
+}
