56ca98fae7ed748668500d73c8bff2dc5930272e
spfforu@gmail.com <root@7l2.aliyun>
2020-07-04 19:32:35 +0800

    add new in 203

--- 203_remove-linked-list-elements.java
+++ 203_remove-linked-list-elements.java
@@ -2,6 +2,8 @@
  * Definition for singly-linked list. public class ListNode { int val; ListNode
  * next; ListNode(int x) { val = x; } }
  */
+
+// 1
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null)
@@ -17,5 +19,23 @@ class Solution {
             cur = cur.next;
         }
         return head;
+    }
+}
+
+// sentinel
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+
+        ListNode prev = sentinel, curr = head;
+        while (curr != null) {
+            if (curr.val == val)
+                prev.next = curr.next;
+            else
+                prev = curr;
+            curr = curr.next;
+        }
+        return sentinel.next;
     }
 }
