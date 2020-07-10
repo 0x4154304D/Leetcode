cffa058ab6d68b2f48099032a591d8c6112416a5
spfforu@gmail.com <root@7l2.aliyun>
2020-07-10 16:50:11 +0800

    Add 200

---
+++ 200_number-of-islands.java
@@ -0,0 +1,73 @@
+//DFS
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+        int numLand = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    ++numLand;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return numLand;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+            return;
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
+
+// BFS
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.offer(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.poll();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.offer((row - 1) * nc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row + 1][col] == '1') {
+                            neighbors.offer((row + 1) * nc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.offer(row * nc + col - 1);
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col + 1] == '1') {
+                            neighbors.offer(row * nc + col + 1);
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return num_islands;
+    }
+}
