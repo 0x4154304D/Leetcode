b43e7049a04da075930946dbf6af8f5687acd4d5
spfforu@gmail.com <root@7l2.aliyun>
2020-07-12 16:16:09 +0800

    DummyNode

--- 21_merge-two-sorted-lists.java
+++ 21_merge-two-sorted-lists.java
@@ -5,35 +5,23 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null)
-            return l2;
-        if (l2 == null)
-            return l1;
-        ListNode cur = null;
-        if (l1.val < l2.val) {
-            cur = l1;
-            l1 = l1.next;
-        } else {
-            cur = l2;
-            l2 = l2.next;
-        }
-        ListNode head = cur;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
                 cur.next = l1;
-                cur = cur.next;
                 l1 = l1.next;
             } else {
                 cur.next = l2;
-                cur = cur.next;
                 l2 = l2.next;
             }
+            cur = cur.next;
+            cur.next = null;
         }
-        if (l1 == null) {
+        if (l1 == null)
             cur.next = l2;
-        } else {
+        else
             cur.next = l1;
-        }
-        return head;
+        return dummy.next;
     }
 }
