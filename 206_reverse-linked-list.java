7cd1f06dc43fe9a13c0ab9b224f036d1ade077dd
spfforu@gmail.com <root@7l2.aliyun>
2020-07-22 10:52:10 +0800

    Add recursive

--- 206_reverse-linked-list.java
+++ 206_reverse-linked-list.java
@@ -2,18 +2,30 @@
  * Definition for singly-linked list. public class ListNode { int val; ListNode
  * next; ListNode(int x) { val = x; } }
  */
+
+ // Iterative
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null)
-            return head;
-        ListNode cur = head.next;
-        head.next = null;
-        while (cur != null) {
-            ListNode tmp = cur;
-            cur = cur.next;
-            tmp.next = head;
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = newHead;
+            newHead = head;
             head = tmp;
         }
-        return head;
+        return newHead;
+    }
+}
+
+
+// Recursive
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
     }
 }
