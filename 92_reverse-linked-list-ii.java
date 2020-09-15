7dbf2e4af793fda2df8003a40669c39e165a525c
spfforu@gmail.com <root@7l2.aliyun>
2020-09-15 10:40:10 +0800

    Clear new way of 92

--- 92_reverse-linked-list-ii.java
+++ 92_reverse-linked-list-ii.java
@@ -1,36 +1,25 @@
-/**
- * Definition for singly-linked list. public class ListNode { int val; ListNode
- * next; ListNode(int x) { val = x; } }
- */
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        if (head == null || head.next == null || m == n)
+        if (m == n)
             return head;
-
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode cur = dummy;
-        for (int i = 0; i < m - 1; ++i) {
-            cur = cur.next;
+        ListNode left = dummy;
+        for (int i = 1; i < m; ++i) {
+            left = left.next;
         }
-
-        ListNode newHead = cur.next;
-        ListNode tail = newHead;
-        ListNode curr = newHead.next;
-        ListNode tmp = null;
-        for (int i = 0; i < n - m; ++i) {
-            if (curr.next != null)
-                tmp = curr.next;
-            else
-                tmp = null;
-            curr.next = newHead;
-            newHead = curr;
-            curr = tmp;
+        ListNode right = left;
+        for (int i = 0; i < (n - m) + 1; ++i) {
+            right = right.next;
         }
-
-        cur.next = newHead;
-        tail.next = curr;
-
+        ListNode tmp = left.next;
+        left.next = right.next;
+        for (int i = 0; i < (n - m + 1); ++i) {
+            ListNode temp = tmp.next;
+            tmp.next = left.next;
+            left.next = tmp;
+            tmp = temp;
+        }
         return dummy.next;
     }
 }
