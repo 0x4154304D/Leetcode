f7425cfeaea7fd5af6ad6442ecb6d3037c0e5d3c
spfforu@gmail.com <root@7l2.aliyun>
2020-09-16 10:18:32 +0800

    203 in dummy node way

--- 203_remove-linked-list-elements.java
+++ 203_remove-linked-list-elements.java
@@ -1,41 +1,15 @@
-/**
- * Definition for singly-linked list. public class ListNode { int val; ListNode
- * next; ListNode(int x) { val = x; } }
- */
-
-// 1
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if (head == null)
-            return null;
-        while (head != null && head.val == val)
-            head = head.next;
-        ListNode cur = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
         while (cur != null) {
             if (cur.next != null && cur.next.val == val) {
                 cur.next = cur.next.next;
-                continue;
+            } else {
+                cur = cur.next;
             }
-            cur = cur.next;
         }
-        return head;
-    }
-}
-
-// sentinel
-class Solution {
-    public ListNode removeElements(ListNode head, int val) {
-        ListNode sentinel = new ListNode(0);
-        sentinel.next = head;
-
-        ListNode prev = sentinel, curr = head;
-        while (curr != null) {
-            if (curr.val == val)
-                prev.next = curr.next;
-            else
-                prev = curr;
-            curr = curr.next;
-        }
-        return sentinel.next;
+        return dummy.next;
     }
 }
