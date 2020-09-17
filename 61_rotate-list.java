43e70457365d7c7b65215ff29045f67339d8378e
spfforu@gmail.com <root@7l2.aliyun>
2020-09-18 00:29:10 +0800

    review 61

--- 61_rotate-list.java
+++ 61_rotate-list.java
@@ -1,7 +1,4 @@
-/**
- * Definition for singly-linked list. public class ListNode { int val; ListNode
- * next; ListNode(int x) { val = x; } }
- */
+//connect a ring
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null || k == 0)
@@ -15,6 +12,7 @@ class Solution {
         k %= size;
         if (k == 0)
             return head;
+
         cur.next = head;
         cur = head;
         for (int i = 0; i < size - k - 1; ++i) {
@@ -23,5 +21,39 @@ class Solution {
         head = cur.next;
         cur.next = null;
         return head;
+    }
+}
+
+// new SingleList
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null)
+            return head;
+        int len = getSize(head);
+        k %= len;
+        if (k == 0)
+            return head;
+        ListNode cur = head;
+        for (int i = 0; i < (len - k) - 1; ++i) {
+            cur = cur.next;
+        }
+        ListNode newHead = cur.next;
+        cur.next = null;
+        cur = newHead;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = head;
+        return newHead;
+    }
+
+    private static int getSize(ListNode head) {
+        ListNode cur = head;
+        int len = 0;
+        while (cur != null) {
+            cur = cur.next;
+            ++len;
+        }
+        return len;
     }
 }
