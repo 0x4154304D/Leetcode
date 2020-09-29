12949f89dc5836d912b8e21dcb27862f5a02d87b
0x4154304D <shipf8023@163.com>
2020-09-29 10:50:53 +0800

    Add 144

--- 144_binary-tree-preorder-traversal.java
+++ 144_binary-tree-preorder-traversal.java
@@ -1,42 +1,38 @@
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
- */
-// Recursive
+// Recursion
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        helper(root, res);
+        preorder(root, res);
         return res;
     }
 
-    private void helper(TreeNode root, List<Integer> res) {
-        if (root != null) {
-            res.add(root.val);
-            helper(root.left, res);
-            helper(root.right, res);
-        }
+    private void preorder(TreeNode root, List<Integer> res) {
+        if (root == null)
+            return;
+        res.add(root.val);
+        preorder(root.left, res);
+        preorder(root.right, res);
     }
 }
 
 // Iterative
 class Solution {
-    public List<Integer> preorderTraversal1(TreeNode root) {
+    public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null)
             return res;
-        Deque<TreeNode> stack = new ArrayDeque<>();
+        Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
-
         while (!stack.isEmpty()) {
             TreeNode cur = stack.pop();
             res.add(cur.val);
             if (cur.right != null) {
                 stack.push(cur.right);
             }
-            if (cur.left = null) {
+            if (cur.left != null) {
                 stack.push(cur.left);
             }
         }
+        return res;
     }
 }
